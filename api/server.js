b2160c3f036d47bfac9b74aae029b871dca3daa7
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-02-10 15:22:06 +0600

    Prisma client CRUD completed

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-import http  from "http";
+import http from "http";
 import express from "express";
 
 import morgan from "morgan";
@@ -7,23 +7,72 @@ import cors from "cors"
 
 require("dotenv").config({})
 
-const {Server}  = require("socket.io")
+const {Server} = require("socket.io")
 
+import {PrismaClient} from '@prisma/client'
+
+const client = new PrismaClient()
 
 const app = express();
+app.use(express.json())
 app.use(morgan("dev"));
 app.use(cors())
 
 
+app.get("/", async (req, res, next) => {
+    res.send("hello")
+})
 
-app.get("/", async (req, res, next)=>{
 
+app.get("/user/:email", async (req, res, next) => {
 
+    try {
+        let user = await client.user.findUnique({
+            where: {email: req.params.email}
+        })
+        res.send(user)
+    } catch (ex) {
+        res.send(ex.message)
+    }
 
 })
 
 
+app.post("/user", async (req, res, next) => {
+    try {
+        const {username, email, password} = req.body
+        let newUser = await client.user.create({
+            data: {
+                username, email, password
+            }
+        })
+        res.send(newUser)
+    } catch (ex) {
+        res.send(ex.message)
+    }
 
+})
+
+
+app.delete("/user/:userId", async (req, res, next) => {
+    try {
+        const {userId} = req.params
+        let deleted = await client.user.delete({
+            where: {
+                id: Number(userId)
+            }
+        })
+        if (deleted) {
+            res.send("User deleted")
+
+        } else {
+            res.send("User not found")
+        }
+    } catch (ex) {
+        res.send(ex.message)
+    }
+
+})
 
 
 const httpServer = http.createServer(app);
@@ -35,10 +84,10 @@ let io = new Server(httpServer, {
 })
 
 
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
     console.log(socket.id, " - connected")
 
-    socket.on("send-message", (payload)=>{
+    socket.on("send-message", (payload) => {
         io.emit("received-msg", payload)
     })
 
@@ -50,6 +99,6 @@ io.on("connection", (socket)=>{
 
 const PORT = 2000
 
-httpServer.listen(PORT, () =>console.info(`server is running on port http://localhost:${PORT}`))
+httpServer.listen(PORT, () => console.info(`server is running on port http://localhost:${PORT}`))
 
 
